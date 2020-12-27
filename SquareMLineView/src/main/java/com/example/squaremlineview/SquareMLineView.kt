4de6794023d99278a9eb0b7348fcd97aaa5bbd4c
Anwesh43 <anweshthecool0@gmail.com>
2020-12-28 02:44:07 +0530

    adding Canvas extension methods

--- SquareMLineView/src/main/java/com/example/squaremlineview/SquareMLineView.kt
+++ SquareMLineView/src/main/java/com/example/squaremlineview/SquareMLineView.kt
@@ -29,3 +29,32 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawSquareMLine(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val mSize : Float = Math.min(w, h) / mSizeFactor
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val sf3 : Float = sf.divideScale(2, parts)
+    save()
+    translate(w / 2, h)
+    drawRect(RectF(-mSize, -mSize * sf1 * 0.5f, mSize, 0f), paint)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        drawLine(-mSize, -mSize / 2, -mSize - size * sf2, -mSize / 2 - size * sf2, paint)
+        drawLine(-mSize - size, 0f, -mSize - size, -size * sf3, paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawSMLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawSquareMLine(scale, w, h, paint)
+}
