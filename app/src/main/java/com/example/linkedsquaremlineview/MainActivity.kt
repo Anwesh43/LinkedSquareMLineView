0464018e752af9e4772da0e0c68a54ac4ebc152a
Anwesh43 <anweshthecool0@gmail.com>
2020-12-28 03:10:32 +0530

    adding view in activity and fullscreen support

--- app/src/main/java/com/example/linkedsquaremlineview/MainActivity.kt
+++ app/src/main/java/com/example/linkedsquaremlineview/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.linkedsquaremlineview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.example.squaremlineview.SquareMLineView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        SquareMLineView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
